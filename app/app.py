071dff89c711402893c60200f04f1d8b266856c7
Anish Sibal <anish.sibal@oracle.com>
2018-08-21 13:55:23 -0500

    foo

---
+++ app/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+import datetime
+app = Flask(__name__)
+now = datetime.datetime.now()
+print(now)
+data = {}
+data = {'siteUpSince': now}
+data['IPs'] = {}
+print(now)
+@app.route('/')
+def uptimeAndIps():
+    ip = request.remote_addr
+    if ip in data['IPs']:
+	data['IPs'][ip] += 1
+    else:
+	data['IPs'][ip] = 1
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
---
+++ compute.tf
@@ -0,0 +1,23 @@
+resource "oci_core_instance" "saltMaster" {
+  #Required
+  availability_domain = "${var.availability_domain}"
+  compartment_id = "${var.compartment_ocid}"
+  shape = "${var.instance_shape}"
+
+  create_vnic_details {
+  	#Required
+  	subnet_id = "${var.subnet_ocid}"
+  	#Optional
+  	assign_public_ip = true
+  }
+  metadata {
+		ssh_authorized_keys = "${var.ssh_authorized_keys}"
+    user_data = "${base64encode(file(var.BootStrapFile))}"
+	}
+  source_details {
+	    #Required
+	    source_type = "image"
+	    source_id = "${var.image_ocid}"
+
+	}
+}
