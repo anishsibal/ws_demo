33ee98186609592eebb55e0451c54446aa55dca6
Anish Sibal <anish.sibal@oracle.com>
2018-08-22 09:34:51 -0500

    Support to read app log on startup. Also added config.py giving the log  location

--- app/app.py
+++ app/app.py
@@ -1,20 +1,36 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+import re
 
 import datetime
 app = Flask(__name__)
+app.config.from_object('config')
+LOG_LOCATION = app.config['LOG_FILE']
 now = datetime.datetime.now()
 data = {}
 data['siteUpSince'] = now
 data['IPs'] = {}
+
+with open(LOG_LOCATION) as fh:
+ for line in fh:
+     ip = line.split()[0]
+     if re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', ip):
+         print(ip)
+         if ip in data['IPs']:
+             data['IPs'][ip] += 1
+         else:
+             data['IPs'][ip] = 1
+     else:
+         continue
+
 @app.route('/')
 def uptimeAndIps():
     ip = request.remote_addr
     if ip in data['IPs']:
-	data['IPs'][ip] += 1
+	    data['IPs'][ip] += 1
     else:
-	data['IPs'][ip] = 1
+	    data['IPs'][ip] = 1
     return jsonify(data)
 
 if __name__ == '__main__':
